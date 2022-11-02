6a0f58ab2584ddb7d3d70ce808a6f7f6fa6d822e
Ian Salt <ian@isalt.digital>
2022-11-02 12:11:43 +0000

    More on utilities

--- src/screens/helpers/untilitys.ts
+++ src/screens/helpers/untilitys.ts
@@ -1,4 +1,50 @@
-export interface UtilityTypes {
-  x: number
+interface Starship {
+  name: string;
+  enableHyperJump: boolean;
+}
+
+const updateStarship = (
+  id: number, 
+  starship: Partial<Starship>
+  ) => {
+};
+
+updateStarship(1, {
+  name: 'Explorer',
+  enableHyperJump: true,
+});
+
+const starships: Record<string, Starship> = {
+  Expolrer1: {
+    name: 'Explorer1',
+    enableHyperJump: true,
+  },
+  Explorer2: {
+    name: 'Explorer2',
+    enableHyperJump: false,
+  }
+}
+
+//  INSTANCE OF
+
+type Constructable<ClassInstance> = new (...args: any[]) => ClassInstance;
+
+function Deletable<BaseClass extends Constructable<{}>> (Base: BaseClass) {
+  return class extends Base {
+    deleted: boolean;
+    delete() {}
+  };
+};
+
+class Car {
+  deleted: boolean = false;
+  delete() {}
+  constructor(public name: string) {}
+}
+
+class User {
+  deleted: boolean = false;
+  delete() {}
+  constructor(public name: string) {}
 }
 
